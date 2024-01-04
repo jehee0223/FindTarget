b528dffa8f68153aac3a062a4ebea06ff1a74145
jehee0223 <dlwpgml0223@gmail.com>
2024-01-04 22:55:55 +0900

    b

--- ml-agents/Project/Assets/ML-Agents/FindTarget/scripts/agent.cs
+++ ml-agents/Project/Assets/ML-Agents/FindTarget/scripts/agent.cs
@@ -108,7 +108,7 @@ public class agent : Agent
                 rotateDir = transform.up * -1f;
                 break;
         }
-        //a
+        //b
         transform.Rotate(rotateDir, Time.deltaTime * 150f);
         Agent.AddForce(dirToGo * 1, ForceMode.VelocityChange);
 
