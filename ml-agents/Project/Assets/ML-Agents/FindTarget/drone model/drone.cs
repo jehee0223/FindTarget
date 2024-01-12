fe7a666d0dc308d13f4ff9ba3fd4893c64d4599a
jehee0223 <dlwpgml0223@gmail.com>
2024-01-13 00:50:12 +0900

    240113 dronemodel

--- ml-agents/Project/Assets/ML-Agents/FindTarget/drone model/drone.cs
+++ ml-agents/Project/Assets/ML-Agents/FindTarget/drone model/drone.cs
@@ -8,6 +8,7 @@ using Unity.MLAgents.Actuators;
 public class DroneAgent : Agent
 {
     public int step = 0;
+    public double reward = 0;
 
     public Rigidbody rb;
     public Rigidbody Rotor_fr;
@@ -18,7 +19,6 @@ public class DroneAgent : Agent
 
     private Vector3 initPosition;
     private Quaternion initQuaternion;
-
 
     // Start is called before the first frame update
     public override void Initialize()
@@ -42,13 +42,13 @@ public class DroneAgent : Agent
         // Move the target to a new spot
         rb.transform.localPosition = new Vector3(0, (float)0.4813456, 0);
         Target.transform.localPosition=new Vector3(Random.Range(-5,5), 9, Random.Range(-5,5));
-        Debug.Log("Ep begin");
+
+        reward = 0;
         SetReward(0f);
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        Debug.Log("collect");
         // Target and Agent positions
         sensor.AddObservation(Target.localPosition);
         sensor.AddObservation(rb.transform.localPosition);
@@ -60,12 +60,11 @@ public class DroneAgent : Agent
         sensor.AddObservation(rb.velocity.z);
     }
 
-    private float power = 50;
+    private float power = 20;
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
         var continuousActions = actionBuffers.ContinuousActions;
-        AddReward(-0.01f);
         if (continuousActions.Length >= 4)
         {
             for (int i = 0; i < 4; i++)
@@ -93,39 +92,77 @@ public class DroneAgent : Agent
         }
         // Actions, size = 4
         // Rewards
+        step++;
         SetReward();
     }
 
+    
     public void SetReward()
     {
-        float distanceToTarget = Vector3.Distance(rb.transform.localPosition, Target.localPosition);
+        /*-----------------------------------Target 찾아가기-------------------------------*/
+        //float distanceToTarget = Vector3.Distance(rb.transform.localPosition, Target.localPosition);
 
-        step++;
-        Debug.Log("step : " + step);
-        Debug.Log("fr : " + Rotor_fr.velocity + "fl : " + Rotor_fl + "br : " + Rotor_br + "bl : " + Rotor_bl);
+        //Debug.Log("distanceToTarget : "+distanceToTarget);
 
-        // Reached target
-        if (distanceToTarget < 0.5f)
+        //// 거리가 먼 만큼 음수보상
+        //AddReward(-(distanceToTarget / 10));
+        //reward -= distanceToTarget / 10;
+
+        //// 몸체의 기울기 x,z축 15도 안넘기
+        //if (Mathf.Abs(rb.transform.localRotation.x) < 15 && Mathf.Abs(rb.transform.localRotation.z) < 15)
+        //{
+        //    AddReward(1f);
+        //    reward += 1;
+        //}
+        //else
+        //{
+        //    AddReward(-1f);
+        //    reward -= 1;
+        //}
+
+        //if (rb.velocity.x < 7 || rb.velocity.y < 7 || rb.velocity.z < 7)
+        //{
+        //    AddReward(-1f);
+        //}
+
+        //// 너무 멀어지면 음수보상 & 종료
+        ////if (distanceToTarget > 20.0f)
+        ////{
+        ////    AddReward(-10f);
+        ////    reward -= 10;
+        ////    Debug.Log("End reward : " + reward);
+        ////    EndEpisode();
+        ////}
+
+        //// 너무 오래걸리면 종료
+        //if (step > 1000)
+        //{
+        //    AddReward(-10);
+        //    Debug.Log("End reward : " + reward);
+        //    EndEpisode();
+        //}
+
+        /*-------------------------------------------------------------------------------------------------*/
+        // 일단 타겟보다 위에 떠있는것 (호버링) 을 먼저 할 수 있도록 해보기(240112)
+        float distance = Mathf.Abs(7 - rb.transform.localPosition.y);
+
+        AddReward(7-distance);
+
+
+        //if (Mathf.Abs(rb.transform.localRotation.x) > 15 || Mathf.Abs(rb.transform.localRotation.z) > 15)
+        //{
+        //    AddReward(-1f);
+        //}
+        //else
+        //{
+        //    AddReward(1f);
+        //}
+
+
+        if (step > 5000)
         {
-            AddReward(10f);
-            EndEpisode();
-            return;
-        }
-
-        SetReward(-distanceToTarget / 10 + Target.transform.position.y - Mathf.Abs(Target.transform.position.y - rb.transform.localPosition.y));
-
-        // Fell off platform
-
-        if (distanceToTarget > 20.0f)
-        {
-            AddReward(-5f);
-            EndEpisode();
-            return;
-        }
-
-        if (step > 1000)
-        {
-            AddReward(-10);
+            AddReward(-10f);
+            Debug.Log("End reward : " + reward);
             EndEpisode();
         }
     }
@@ -162,4 +199,13 @@ public class DroneAgent : Agent
         }
     }
 
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("target"))
+        {
+            SetReward(10f);
+            Debug.Log("success");
+            EndEpisode();
+        }
+    }
 }
